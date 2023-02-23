04c38736e07a66b03d35766ccb0a847a4bf2537c
Yatsuuw <121401515+Yatsuuw@users.noreply.github.com>
2023-02-23 21:17:17 +0100

    Update ball.py

--- ball.py
+++ ball.py
@@ -3,31 +3,31 @@ from random import randint
 BLACK = (0, 0, 0)
 
 
-#class
+# Class
 class Ball(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
 
-        #set
+        # Set
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
 
-        #draw ball
+        # Draw ball
         pygame.draw.rect(self.image, color, [0, 0, width, height])
 
-        #velocity
+        # Velocity
         self.velocity = [randint(4,8),randint(-8,8)]
 
         #fetch rectangle
         self.rect = self.image.get_rect()
 
-    #update
+    # Update
     def update(self):
         self.rect.x += self.velocity[0]
         self.rect.y += self.velocity[1]
 
-    #bounce
+    # Bounce
     def bounce(self):
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = randint(-8,8)
