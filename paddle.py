9a8ef3e133f86d9f9f585227d8fa59de9d7085f1
Yatsuuw <121401515+Yatsuuw@users.noreply.github.com>
2023-02-23 21:16:51 +0100

    Update paddle.py

--- paddle.py
+++ paddle.py
@@ -1,29 +1,29 @@
 import pygame
 BLACK = (0, 0, 0)
 
-#class
+# Class
 class Paddle(pygame.sprite.Sprite):
     def __init__(self, color, width, height):
         super().__init__()
 
-        #set
+        # Set
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
         self.image.set_colorkey(BLACK)
 
-        #draw paddle
+        # Draw paddle
         pygame.draw.rect(self.image, color, [0, 0, width, height])
 
-        #fetch rectangle
+        # Fetch rectangle
         self.rect = self.image.get_rect()
 
-    #moving up
+    # Moving up
     def moveUp(self, pixels):
         self.rect.y -= pixels
         if self.rect.y < 0:
             self.rect.y = 0
 
-    #moving down
+    # Moving down
     def moveDown(self, pixels):
         self.rect.y += pixels
         if self.rect.y > 400:
